9aaff1802f9bc793419e9d57b309d50ac38655e8
alirezaseif20 <alirezaariye@gmail.com>
2024-05-11 21:18:47 +0330

    feat: use Sliver Quilted Grid Delegate

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -8,43 +9,73 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xff1C1F2E),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 46,
-              margin: const EdgeInsets.only(left: 18, right: 18, top: 12),
-              decoration: const BoxDecoration(
-                color: Color(0xff272B40),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(13),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  children: [
-                    Image.asset('assets/images/icon_search.png'),
-                    const SizedBox(width: 8),
-                    const Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search User',
-                          hintStyle: TextStyle(color: Colors.white),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    Image.asset('assets/images/icon_scan.png'),
-                    const SizedBox(width: 15),
-                  ],
-                ),
-              ),
-            ),
-            _categoryList(),
-          ],
+        child: GridView.custom(
+          gridDelegate: SliverQuiltedGridDelegate(
+              crossAxisCount: 3,
+              mainAxisSpacing: 3,
+              crossAxisSpacing: 3,
+              repeatPattern: QuiltedGridRepeatPattern.inverted,
+              pattern: [
+                const QuiltedGridTile(2, 1),
+                const QuiltedGridTile(2, 2),
+                const QuiltedGridTile(1, 1),
+                const QuiltedGridTile(1, 1),
+                const QuiltedGridTile(1, 1),
+              ]),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Container(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),child: Center(child: Text('$index')),
+              );
+            },
+            childCount: 10,
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _getContent() {
+    return Column(
+      children: [
+        Container(
+          height: 46,
+          margin: const EdgeInsets.only(left: 18, right: 18, top: 12),
+          decoration: const BoxDecoration(
+            color: Color(0xff272B40),
+            borderRadius: BorderRadius.all(
+              Radius.circular(13),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                Image.asset('assets/images/icon_search.png'),
+                const SizedBox(width: 8),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search User',
+                      hintStyle: TextStyle(color: Colors.white),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Image.asset('assets/images/icon_scan.png'),
+                const SizedBox(width: 15),
+              ],
+            ),
+          ),
+        ),
+        _categoryList(),
+      ],
     );
   }
 
