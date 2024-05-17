1892c9522d419cc7ee0fbe4029844a30c7127137
alirezaseif20 <alirezaariye@gmail.com>
2024-05-17 23:53:44 +0330

    feat: add Sliver Appbar & Tabbar

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone/constants/colors.dart';
 
 class UserProfileScreen extends StatelessWidget {
@@ -8,48 +9,155 @@ class UserProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: CustomColor.scaffoldBg,
-        body: NestedScrollView(
-          headerSliverBuilder: (context, isScrolled) {
-            return [
-              SliverAppBar(
-                toolbarHeight: 80,
-                actions: const [
-                  Padding(
-                    padding: EdgeInsets.only(right: 18, top: 18),
-                    child: Icon(
-                      Icons.menu,
-                      color: Colors.white,
+        body: DefaultTabController(
+          length: 2,
+          child: NestedScrollView(
+            headerSliverBuilder: (context, isScrolled) {
+              return [
+                SliverAppBar(
+                  toolbarHeight: 80,
+                  actions: const [
+                    Padding(
+                      padding: EdgeInsets.only(right: 18, top: 18),
+                      child: Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ],
-                bottom: PreferredSize(
-                  preferredSize: const Size.fromHeight(10),
-                  child: Container(
-                    height: 14,
-                    decoration: const BoxDecoration(
-                      color: CustomColor.scaffoldBg,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
+                  ],
+                  bottom: PreferredSize(
+                    preferredSize: const Size.fromHeight(10),
+                    child: Container(
+                      height: 14,
+                      decoration: const BoxDecoration(
+                        color: CustomColor.scaffoldBg,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                expandedHeight: 170,
-                backgroundColor: CustomColor.scaffoldBg,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.asset(
-                    'assets/images/item1.png',
-                    fit: BoxFit.cover,
+                  expandedHeight: 170,
+                  backgroundColor: CustomColor.scaffoldBg,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.asset(
+                      'assets/images/item1.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: _getHeaderProfile(),
-              )
-            ];
-          },
-          body: const Text('h'),
+                SliverToBoxAdapter(
+                  child: _getHeaderProfile(),
+                ),
+                SliverPersistentHeader(
+                  pinned: true,
+                  floating: true,
+                  delegate: TabBarViewDelegate(
+                    TabBar(
+                      dividerHeight: 0,
+                      indicatorWeight: 2,
+                      indicatorColor: CustomColor.red1,
+                      indicatorPadding: const EdgeInsets.only(bottom: 0),
+                      tabs: [
+                        Tab(
+                          icon: Image.asset('assets/images/icon_tab_posts.png'),
+                        ),
+                        Tab(
+                          icon: Image.asset(
+                              'assets/images/icon_tab_bookmark.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ];
+            },
+            body: TabBarView(
+              children: [
+                CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.only(top: 20),
+                      sliver: SliverGrid(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return Container(
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                child: FittedBox(
+                                    fit: BoxFit.cover,
+                                    child: Image.asset(
+                                        'assets/images/item$index.png')),
+                              ),
+                            );
+                          },
+                          childCount: 10,
+                        ),
+                        gridDelegate: SliverQuiltedGridDelegate(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 3,
+                            crossAxisSpacing: 3,
+                            repeatPattern: QuiltedGridRepeatPattern.inverted,
+                            pattern: [
+                              const QuiltedGridTile(1, 1),
+                              const QuiltedGridTile(1, 1),
+                              const QuiltedGridTile(1, 1),
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+                CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.only(top: 20),
+                      sliver: SliverGrid(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return Container(
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                child: FittedBox(
+                                    fit: BoxFit.cover,
+                                    child: Image.asset(
+                                        'assets/images/item$index.png')),
+                              ),
+                            );
+                          },
+                          childCount: 10,
+                        ),
+                        gridDelegate: SliverQuiltedGridDelegate(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 3,
+                            crossAxisSpacing: 3,
+                            repeatPattern: QuiltedGridRepeatPattern.inverted,
+                            pattern: [
+                              const QuiltedGridTile(1, 1),
+                              const QuiltedGridTile(1, 1),
+                              const QuiltedGridTile(1, 1),
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ));
   }
 
@@ -109,5 +217,30 @@ class UserProfileScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class TabBarViewDelegate extends SliverPersistentHeaderDelegate {
+  TabBarViewDelegate(this._tabBar);
+
+  TabBar _tabBar;
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: CustomColor.scaffoldBg,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
